e3fd0c86a694f25ca2a021f95768ed8a3b20fcbb
gio <giovasmorgado@gmail.com>
2026-09-15 11:30:30 -0300

    Alterado por Texuguex

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Estrutura de Dados - Fatec (2026/2_)" << endl;
+	cout << "Estrutura de Dados - Fatec (2026/2)" << endl;
 
 	
 	cout << "Tipos Numericos\n";
