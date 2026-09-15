1c05b2580ce97d0550f008f894c9726e62356c1a
gio <giovasmorgado@gmail.com>
2026-09-15 12:59:25 -0300

    Até o exercício 3 feito

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main()
 {
+
+#include <climits>
+	cout << "int  max = " << INT_MAX << "\n";
+	cout << "int  min = " << INT_MIN << "\n";
+	cout << "short max = " << SHRT_MAX << "\n";
+
 	cout << "Estrutura de Dados - Fatec (2026/2)" << endl;
 
 	
@@ -59,17 +65,41 @@ int main()
 	int dias = 0;
 	int horas = 0;
 	int minutos = 0;
+	double graus = 0;
+	int fahrenheit = 0;
+	int kelvin = 0;
+	long segundos = 0;
+	int segdias = 0;
+	int seghoras = 0;
+	int segminutos = 0;
+
+
 
 	cout << "Digite seu nome: ";
 	cin >> nome;
 	cout << "Digite sua idade: ";
 	cin >> idade;
+	cout << "Digite quantos graus estao fazendo hoje: ";
+	cin >> graus;
+	cout << "Digite um número em segundos: ";
+	cin >> segundos;
 	dias = idade * 365;
 	horas = dias * 24;
 	minutos = horas  * 60;
+	fahrenheit = graus * 1.8 + 32;
+	kelvin = graus + 253,15;
+	segdias = segundos / 864000;
+	seghoras = segundos / 3600;
+	segminutos = segundos / 60;
+
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
+	cout << "Seus graus em Fahrenheit ficam: " << fahrenheit << endl;
+	cout << "Seus graus em Kelvin ficam: " << kelvin << endl;
+	cout << "Seus segundos em dias ficam: " << segdias << endl;
+	cout << "Seus segundos em horas ficam: " << seghoras << endl;
+	cout << "Seus segundos em minutos ficam: " << segminutos << endl;
 
 	cout << "Qualquer tecla para sair...";
 	system("pause"); // somente no Windows
